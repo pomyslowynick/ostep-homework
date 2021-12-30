3b0bfdbc938238f43f9a5a291a31dc8eb79f74d8
pomyslowynick <rafal.rosa@mycit.ie>
2021-12-30 16:18:28 +0000

    Add file desriptor homework2

---
+++ cpu-api/hw2.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    // process a
+    FILE *fp = open("README.md", O_RONLY);
+    char* line = NULL;
+    size_t len = 0;
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Error forking\n");
+        exit(1);
+    } else if (rc == 0) {
+        getline(&line, &len, fp)
+        printf("%s", line);
+    } else {
+        wait(NULL);
+        getline(&line, &len, fp)
+        printf("%s", line);
+    }
+    return 0;
+}
+
