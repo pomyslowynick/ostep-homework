be9fb28044cb65ad0c1b0d7372c7889f9b44009d
pomyslowynick <rafal.rosa@mycit.ie>
2022-01-01 14:31:28 +0000

    Add hw5

---
+++ cpu-api/hw5.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    // process a
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Error forking\n");
+        exit(1);
+    } else if (rc == 0) {
+        int ret = wait(NULL);
+        printf("Child wait ret %d\n", ret);
+    } else {
+        int ret = wait(NULL);
+        printf("Parent wait ret %d\n", ret);
+    }
+    return 0;
+}
+
