41911fd517cf4dae76bb9cbb2d51c713c5bfc477
pomyslowynick <rafal.rosa@mycit.ie>
2022-01-01 14:22:09 +0000

    Add corrected versions of exec

--- cpu-api/hw4.c
+++ cpu-api/hw4.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
+#include <features.h>
 
 int main(int argc, char *argv[]) {
     // process a
@@ -18,34 +18,30 @@ int main(int argc, char *argv[]) {
         myargsls[0] = strdup("ls");
         myargsls[1] = NULL;
 
-        char* myargsEcho[3];
-        myargsEcho[0] = strdup("echo");
-        myargsEcho[1] = strdup("$HELLO");
-        myargsEcho[1] = NULL;
+        const char *env[] = { "HELLO=Hello", (char *)NULL };
 
-        char* env[3];
-        env[0] = strdup("HELLO");
-        env[1] = strdup("Hello world!");
-        env[2] = NULL;
 
         printf("execl output: \n");
-        execl("/bin/ls", myargsls);
+        //execl("/bin/ls", myargsls[0], (char *)NULL);
         printf("-------------\n");
 
         printf("execlp output: \n");
-        execlp("ls", myargsls);
+        //execlp("echo", myargsEcho[0], myargsEcho[1], (char *)NULL);
         printf("-------------\n");
 
         printf("execle output: \n");
-        execle("/bin/echo", myargsEcho, env);
+        // interesting point, the variable expansion is done by the shell, so executing this without
+        // the shell doesn't print the right env variable
+        //execle("/bin/bash","bash", "-c", "echo $HELLO", (char *)NULL, env);
         printf("-------------\n");
 
         printf("execvp output: \n");
-        execvp("ls", myargsls);
+        //execvp("ls", myargsls);
         printf("-------------\n");
 
         printf("execvpe output: \n");
-        execvpe("echo", myargsEcho, env);
+        //execvpe does not work on my system, or I haven't found a way to run on Ubuntu16.04
+        //execvpe("/bin/bash","bash", "-c", "echo $HELLO", (char *)NULL, env);
         printf("-------------\n");
 
         printf("execv output: \n");
@@ -56,4 +52,3 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
-
