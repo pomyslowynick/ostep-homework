e3dfa90b9dfa9ea72365e7e264f66cf5d37f7691
pomyslowynick <rafal.rosa@mycit.ie>
2022-01-01 11:52:07 +0000

    Add examples of different execs

--- cpu-api/hw4.c
+++ cpu-api/hw4.c
@@ -14,11 +14,43 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Error forking\n");
         exit(1);
     } else if (rc == 0) {
-        char* myargs[3];
-        myargs[0] = strdup("ls");
-        myargs[1] = NULL;
+        char* myargsls[2];
+        myargsls[0] = strdup("ls");
+        myargsls[1] = NULL;
 
+        char* myargsEcho[3];
+        myargsEcho[0] = strdup("echo");
+        myargsEcho[1] = strdup("$HELLO");
+        myargsEcho[1] = NULL;
+
+        char* env[3];
+        env[0] = strdup("HELLO");
+        env[1] = strdup("Hello world!");
+        env[2] = NULL;
+
+        printf("execl output: \n")
+        execl("/bin/ls", myargs);
+        printf("-------------\n")
+
+        printf("execlp output: \n")
+        execlp("ls", myargs);
+        printf("-------------\n")
+
+        printf("execle output: \n")
+        execle("/bin/echo", myargsEcho, env);
+        printf("-------------\n")
+
+        printf("execvp output: \n")
         execvp("ls", myargs);
+        printf("-------------\n")
+
+        printf("execvpe output: \n")
+        execvpe("echo", myargsEcho, env);
+        printf("-------------\n")
+
+        printf("execv output: \n")
+        execv("/bin/ls", myargs);
+        printf("-------------\n")
     } else {
         wait(NULL);
     }
