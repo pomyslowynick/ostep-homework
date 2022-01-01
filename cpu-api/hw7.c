88e0923b2465d2f34c40dbf8fb252ca6a20db438
pomyslowynick <rafal.rosa@mycit.ie>
2022-01-01 15:08:55 +0000

    Add homework 7

---
+++ cpu-api/hw7.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    // process a
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Error forking\n");
+        exit(1);
+    } else if (rc == 0) {
+        close(STDOUT_FILENO);
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+    } else {
+        wait(NULL);
+    }
+    return 0;
+}
